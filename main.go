548cd005a1b89b06826c22c9a16684bfe68199b2
logeshwhatever <logeshwhatever@gmail.com>
2024-12-16 06:23:52 +0000

    B: Created a connection on localhost 3000

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/logeshwhatever/foreverstore/p2p"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	tr := p2p.NewTCPTransport(":3000")
+
+	if err := tr.ListenAndAccept(); err != nil {
+		log.Fatal(err)
+	}
+	select {}
 }
