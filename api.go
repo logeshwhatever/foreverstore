aaa4925e719a7608a5f7f75ca7c93c2d5244879f
logeshwhatever <logeshwhatever@gmail.com>
2024-12-16 06:00:15 +0000

    A: Setup boilerplate TCP server for the project with other peer connections

---
+++ api.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func WriteJSON(w http.ResponseWriter, status int, v any) error {
+	w.WriteHeader(status)
+	w.Header().Set("Content-Type", "application/json")
+	return json.NewEncoder(w).Encode(v)
+}
+
+type apiFunc func(w http.ResponseWriter, r *http.Request) error
+
+type ApiError struct {
+	Error string
+}
+
+func makeHTTPHandlefunc(f apiFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := f(w, r); err != nil {
+			WriteJSON(w, http.StatusBadRequest, ApiError{Error: err.Error()})
+		}
+	}
+}
+
+type APIServer struct {
+	listenAddress string
+}
+
+func NewAPIServer(listenAddress string) *APIServer {
+	return &APIServer{
+		listenAddress: listenAddress,
+	}
+}
+
+func (s *APIServer) Run() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/account", makeHTTPHandlefunc(s.handleAccount))
+	router.HandleFunc("/account/{id}", makeHTTPHandlefunc(s.handleGetAccount))
+
+	log.Println("Server starting on port:", s.listenAddress)
+
+	http.ListenAndServe(s.listenAddress, router)
+}
+
+func (s *APIServer) handleAccount(w http.ResponseWriter, r *http.Request) error {
+
+	if r.Method == "GET" {
+		return s.handleGetAccount(w, r)
+	}
+	if r.Method == "POST" {
+		return s.handleCreateAccount(w, r)
+	}
+	if r.Method == "DELETE" {
+		return s.handleDeleteAccount(w, r)
+	}
+	return nil
+}
+
+func (s *APIServer) handleGetAccount(w http.ResponseWriter, r *http.Request) error {
+	id := mux.Vars(r)["id"]
+	fmt.Println(id)
+	WriteJSON(w, http.StatusOK, &Account{})
+	return nil
+}
+
+func (s *APIServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func (s *APIServer) handleDeleteAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func (s *APIServer) handleTransfer(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
---
+++ p2p/tcp_transport.go
@@ -0,0 +1,61 @@
+package p2p
+
+import (
+	"fmt"
+	"net"
+	"sync"
+)
+
+type TCPPeer struct {
+	conn net.Conn
+	// if we dial and retrieve a connection, outbound == true
+	// if we accept and retrieve a connection, outbound == false
+	outbound bool
+}
+
+type TCPTransport struct {
+	listenAddress string
+	listener      net.Listener
+	mu            sync.RWMutex
+	peers         map[net.Addr]Peer
+}
+
+func NewTCPPeer(conn net.Conn, outbound bool) *TCPPeer {
+	return &TCPPeer{
+		conn:     conn,
+		outbound: outbound,
+	}
+}
+
+func NewTCPTransport(listenAddr string) *TCPTransport {
+	return &TCPTransport{
+		listenAddress: listenAddr,
+	}
+}
+
+func (t *TCPTransport) ListenAndAccept() error {
+	var err error
+
+	t.listener, err = net.Listen("tcp", t.listenAddress)
+	if err != nil {
+		return err
+	}
+
+	go t.startAcceptLoop()
+	return nil
+}
+
+func (t *TCPTransport) startAcceptLoop() {
+	for {
+		conn, err := t.listener.Accept()
+		if err != nil {
+			fmt.Printf("TCP: Accept error %s\n", err)
+		}
+		go t.handleConn(conn)
+	}
+}
+
+func (t *TCPTransport) handleConn(conn net.Conn) {
+	peer := NewTCPPeer(conn, true)
+	fmt.Printf("new incoming connection %+v\n", peer)
+}
